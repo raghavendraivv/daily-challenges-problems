df327eba0cd24e64534bc06107a12d90581c61dd
raghavraghs <44517942+raghavraghs@users.noreply.github.com>
2019-10-12 00:29:24 +0530

    Update and rename the sum.c to the sum.cpp

--- the sum.cpp
+++ the sum.cpp
@@ -6,7 +6,8 @@ int main()
     {
     
          int n,sum=0,a=0,b=1,c=0,i;
-         scanf("%d",&n);
+         //scanf("%d",&n);
+         cin  >> n;
          for(a=0;a<n;a++)
          {
              a=b+c;
@@ -14,6 +15,7 @@ int main()
              c=a;
              sum=sum+a;
          }
-         printf("%d",sum);
+         cout << sum;        
+ //printf("%d",sum);
     return 0;
 }
