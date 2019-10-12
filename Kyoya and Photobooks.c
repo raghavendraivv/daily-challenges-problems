628413b8dff799103b1117339d924938f196de5d
dvnsandeep <43857074+dvnsandeep@users.noreply.github.com>
2019-10-12 18:30:29 +0530

    Update Kyoya and Photobooks.c

--- Kyoya and Photobooks.c
+++ Kyoya and Photobooks.c
@@ -1,23 +1,19 @@
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
-
-
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
 
 int main() 
 {
-    char a[20];
-    scanf("%s",a);
-    int k = strlen(a);
+    string a[20];
+    //scanf("%s",a);
+    cin >> a;
+    int k = a.length();
     int n = 26,i;
     for(i=0;i<k;i++)
     {
         n = n + 25;
     }
-    printf("%d",n);
-
+   // printf("%d",n);
+cout << n;
     return 0;
 }
-
-
