15dfad63c617c6f74d7dc2f6ddec2631e6934c6b
chandu <32795376+chanduch1999@users.noreply.github.com>
2021-10-02 12:06:44 +0530

    Update Different is good.cpp

--- Different is good.cpp
+++ Different is good.cpp
@@ -1,8 +1,7 @@
 By ivvraghavendra199, contest: Codeforces Round #352 (Div. 2), problem: (B) Different is Good, Accepted, #
- #include <bits/stdc++.h>
 
+#include <bits/stdc++.h>
 using namespace std;
-
 
 
 int main()
@@ -15,13 +14,17 @@ int main()
         cout << -1;
         return 0;
     }
+ 
     string a;
     int i,j,count=0;
     cin >> a;
+ 
     for(i=0;i<a.size();i++)
     {
         x.insert(a[i]);
     }
+ 
     cout << n - x.size();
+ 
     return 0;
 }
